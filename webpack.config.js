4dde421f75be91e6b233016e3c4b750af3647b9e
kadeshiro <gaaadaaa16@gmail.com>
2020-09-18 14:42:44 +0900

    [chor]webpack-bundle-analyzerを追加

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,23 @@ const path = require("path")
 const HtmlPlugin = require("html-webpack-plugin")
 const TsConfigWebpackPlugin = require("ts-config-webpack-plugin")
 const ScssConfigWebpackPlugin = require("scss-config-webpack-plugin")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin
 
 require("dotenv").config()
+
+const plugins = [
+  new TsConfigWebpackPlugin(),
+  new ScssConfigWebpackPlugin(),
+  new HtmlPlugin({
+    template: "./src/index.html",
+  }),
+  new webpack.EnvironmentPlugin(["API_KEY"]),
+].concat(process.env.NODE_ENV === "analyze" ? new BundleAnalyzerPlugin() : [])
 
 module.exports = {
   devServer: {
     contentBase: [path.join(__dirname, "dist")],
   },
-  plugins: [
-    new TsConfigWebpackPlugin(),
-    new ScssConfigWebpackPlugin(),
-    new HtmlPlugin({
-      template: "./src/index.html",
-    }),
-    new webpack.EnvironmentPlugin(["API_KEY"]),
-  ],
+  plugins,
 }
